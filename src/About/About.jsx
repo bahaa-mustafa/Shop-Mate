97262c49f6b5c4c8f243e0781dafc73a3d5beca7
bahaa-mustafa <bahaamustafabadr@gmail.com>
2025-09-13 20:21:55 +0300

    cart responsive

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import "./About.css"
 import { useDispatch, useSelector } from "react-redux";
 import { decreaseQuantity, increaseQuantity, removeFromCart } from "../Redux/cartSlice";
@@ -16,27 +16,28 @@ function About() {
   // let [totalPrice, setTotalPrice] = useState(0)
 
   const totalPrice = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
-  const totalQuantity = items.reduce((acc, item)=> acc + item.quantity, 0);
+  const totalQuantity = items.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <>
-      {(items.length === 0) ? <div className="about"><h2>Cart is empty</h2></div> : <div className="carts">
+      {(items.length === 0) ? <div className="about"><h2>Cart is empty</h2><NavLink to={"/products"}>Shop now</NavLink></div> : <div className="carts">
         <div className="total">
           <p>Total items: {totalQuantity}</p>
           <p>Total price: ${totalPrice.toFixed(2)}</p>
-          </div>
+        </div>
         {items.map((item) => (<div className="cart" key={item.id}>
           <div className="img"> <img src={item.image} alt={item.image} /></div>
-          <div className="text">
-            <h2>{item.title}</h2>
-            <h3>${item.price}</h3>
-          </div>
-          <div className="actions">
-            <button className="inc" onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
-            <p>{item.quantity}</p>
-            <button className="dec" onClick={() => dispatch(decreaseQuantity(item.id))}>-</button>
-            <button className="del" onClick={() => dispatch(removeFromCart(item.id))}><Trash2 /></button>
-          </div>
+          <div className="containt">
+            <div className="text">
+              <h2>{item.title}</h2>
+              <h3>${item.price}</h3>
+            </div>
+            <div className="actions">
+              <button className="inc" onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
+              <p>{item.quantity}</p>
+              <button className="dec" onClick={() => dispatch(decreaseQuantity(item.id))}>-</button>
+              <button className="del" onClick={() => dispatch(removeFromCart(item.id))}><Trash2 /></button>
+            </div></div>
         </div>))}
       </div>}
 
